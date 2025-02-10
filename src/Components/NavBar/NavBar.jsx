3e8767e67687d8adf6435de89f276e6e48e78ff2
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2025-02-11 01:10:43 +0330

    The upper part of the menu was made

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,24 @@
 import navbarStyle from "./NavBar.module.scss"
+import { IoSearch } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
 const NavBar = () => {
     return ( <>
     
-
+        <nav>
+            <div className={navbarStyle.navbarTop}>
+            <img id={navbarStyle.logo} src="../../../public/Images/logo.svg" alt="logo" />
+            <div className={navbarStyle.navbarTopcenter}>
+            <input id={navbarStyle.btn} type="text" name="Search" placeholder="جستجو" />
+            <IoSearch className={navbarStyle.icon} />
+            </div>
+            <div className={navbarStyle.navbarToprLedft}>
+            <IoPersonOutline className={navbarStyle.adminPanelIcon}/>
+            <MdOutlineShoppingCart className={navbarStyle.CartIcon}/>
+            </div>
+            </div>
+        </nav>
     
     
     </> );
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./Components/App/App";
+import "./Style.module.scss";
 
 createRoot(document.getElementById("root")).render(<App />);
