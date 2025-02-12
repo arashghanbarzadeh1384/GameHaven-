c0cb5597496631bcc164377bd0cfa5931d96bb12
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2025-02-12 09:27:02 +0330

    maked frsit megaMenu

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import navbarStyle from "./NavBar.module.scss"
+import navbarStyle from "./NavBar.module.scss";
 import { IoSearch } from "react-icons/io5";
 import { IoPersonOutline } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
@@ -7,37 +7,56 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { MyContext } from "../App/App";
 
 const NavBar = () => {
-    const navBarData = useContext(MyContext)
-    return ( <>
-    
-        <nav>
-            <div className={navbarStyle.navbarTop}>
-            <img id={navbarStyle.logo} src="../../../public/Images/logo.svg" alt="logo" />
-            <div className={navbarStyle.navbarTopcenter}>
-            <input id={navbarStyle.btn} type="text" name="Search" placeholder="جستجو" />
+  const navBarData = useContext(MyContext);
+  return (
+    <>
+      <nav>
+        <div className={navbarStyle.navbarTop}>
+          <img
+            id={navbarStyle.logo}
+            src="../../../public/Images/logo.svg"
+            alt="logo"
+          />
+          <div className={navbarStyle.navbarTopcenter}>
+            <input
+              id={navbarStyle.btn}
+              type="text"
+              name="Search"
+              placeholder="جستجو"
+            />
             <IoSearch className={navbarStyle.icon} />
-            </div>
-            <div className={navbarStyle.navbarToprLedft}>
-            <IoPersonOutline className={navbarStyle.adminPanelIcon}/>
-            <MdOutlineShoppingCart className={navbarStyle.CartIcon}/>
-            </div>
-            </div>
-            <div className={navbarStyle.navbarBotom}>
-              {navBarData?.map((item) => {
-                return (
+          </div>
+          <div className={navbarStyle.navbarToprLedft}>
+            <IoPersonOutline className={navbarStyle.adminPanelIcon} />
+            <MdOutlineShoppingCart className={navbarStyle.CartIcon} />
+          </div>
+        </div>
+        <div className={navbarStyle.navbarBotom}>
+          {navBarData?.map(item => {
+            return (
+              <ul key={item.id}>
+                <li>
+                  <AiOutlineMenu className={navbarStyle.menuIcon} />
+                  <a href="#">{item.name}</a>
+                  <div className={navbarStyle.subMenu}>
                     <ul key={item.id}>
-                        <li>
-                        <AiOutlineMenu className={navbarStyle.menuIcon}/>
-                            <a href="#">{item.name}</a>
-                        </li>
+                      {item.dropDown?.map(iitem => {
+                        return (
+                          <li key={iitem.id}>
+                            <a href="#">{iitem.name}</a>
+                          </li>
+                        );
+                      })}
                     </ul>
-                )
-              })}
-            </div>
-        </nav>
-    
-    
-    </> );
-}
- 
+                  </div>
+                </li>
+              </ul>
+            );
+          })}
+        </div>
+      </nav>
+    </>
+  );
+};
+
 export default NavBar;
